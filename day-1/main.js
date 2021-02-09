c71dcbc3a735dc4eab024ba3b40ec2a398c5f10a
robert1611 <76051413+robert1611@users.noreply.github.com>
2021-02-09 07:59:33 -0600

    Add files via upload

--- day-1/main.js
+++ day-1/main.js
@@ -1,18 +1,51 @@
-// Basics 0f Javascript
-/*
+let dog_string = "Hello Max, my name is DOG, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
 
-Multiline Comment in Javascript
-- variable Declaraton in JS
+function findWords(s, names){
+    let ret = []
+    for(let i = 0; i < names.length; i++){
+        if(s.includes(names[i])) {
+            ret.push(names[i])
+        }
+    }
+    return ret
+}
 
-primitive types StringList, integers, Boolean, floats, arrays, objects(aka dicts)
-functions in JS
-Loops in JS
+console.log('found words', findWords(dog_string, dog_names))
 
-*/
+let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger",'last']
+function replaceEvens(arr){
+    for(let i = 0; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            arr.splice(i, 1, 'even index')
+        }
+    }
+    return arr
 
-//String Variable
-var first_name = "Bill"
+}
 
-//Print it
-console.log(first_name) 
+/*for i in range(len(arr)):
+    if i % 2 == 0:
+        arr[i] = 'even index'*/
 
+console.log(replaceEvens(arr))
+
+//for name in dog_names:  
+//    if name in dog_string
+
+/* Code wars BMI */
+
+function determine_BMI(BMI) {
+    if (BMI <= 18.5 ) {
+        return 'Underweight'
+     } else if (BMI >18.5 && BMI <= 25) {
+        return 'Normal'
+    } else if (BMI > 25 && BMI <=30) {
+      return "Overweight' 
+    } else {
+        return "Obese"
+    }
+    
+}
+
+console.log(determine_BMI(22)
